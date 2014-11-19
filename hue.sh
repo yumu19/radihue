b4337da4d3c1cc699734742fcaa5dab370bdc9cc
yumu19 <yumu.stp@gmail.com>
2014-11-20 03:09:51 +0900

    update

--- hue.sh
+++ hue.sh
@@ -1,6 +1,16 @@
 IPADDR="192.168.0.6"
-NUM="2"
-HUE=$1
-QUERY="{\"on\":true, \"sat\":255, \"bri\":255, \"hue\":${HUE}}"
+NUM=$1
+SAT=$2
+BRI=$3
+HUE=$4
+i=0
+QUERY="{\"on\":true, \"sat\":${SAT}, \"bri\":${BRI}, \"hue\":${HUE}}"
 echo ${QUERY}
+while [ $i -lt 3 ]
+do 
+	curl -v -H "Content-type: application/json" -X PUT -d "${QUERY}" http://${IPADDR}/api/newdeveloper/lights/${NUM}/state
+	sleep 1s
+	i=`expr $i + 1`
+done
+QUERY="{\"on\":true, \"sat\":0, \"bri\":0, \"hue\":${HUE}}"
 curl -v -H "Content-type: application/json" -X PUT -d "${QUERY}" http://${IPADDR}/api/newdeveloper/lights/${NUM}/state
--- radihue.js
+++ radihue.js
@@ -1,22 +1,42 @@
 var http = require('http');
 var url  = require('url');
 var exec = require('child_process').exec;
+var i = 0;
+var j = 1;
+var command = "";
 
 http.createServer(function (req, res) {
 	var parts = url.parse(req.url,true);
     var type = parts.query.type;
     switch(type) {
-    	case 'hee':
+    	case 'hee': //orange
+    		command = "./hue.sh "+j+" 255 255 10000"
+	    	exec(command, function(err, stdout, stderr) {});
+	    	console.log(j+",hee");
     		break;
-    	case 'great':
-	    	exec('pwd -P', function(err, stdout, stderr) {});
-	    	console.log("great");
+    	case 'great': //red
+	    	command = "./hue.sh "+j+" 255 255 65535"
+	    	exec(command, function(err, stdout, stderr) {});
+	    	console.log(j+",great");
     		break;
-    	case 'www':
+    	case 'www': //green
+ 	    	command = "./hue.sh "+j+" 255 255 25500"
+ 		   	exec(command, function(err, stdout, stderr) {});
+	    	console.log(j+",www");
+    		break;
+    	case 'question': //blue
+    		command = "./hue.sh "+j+" 255 255 47000"
+ 		   	exec(command, function(err, stdout, stderr) {});
+	    	console.log(j+",question");
     		break;
     	default:
     		break;
     }
+    i++;
+    if(i>2) {
+    	i=0;
+    }
+    j = i+1
     res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
     res.end('<h1>OK</h1>');
-}).listen(1337, "localhost");
+}).listen(1337, "192.168.0.3");
